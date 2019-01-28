3829611bc2e82165adbd3ca1a0bf50ebcf6944ea
Chris Bird <drkvogel@yahoo.co.uk>
2019-01-28 01:54:04 +0000

    more

---
+++ dev/cruft.py
@@ -0,0 +1,13 @@
+
+# print('test1', file=sys.stderr) # prints to console
+
+    # print('test2', file=sys.stderr)
+    # print(json_data, file=sys.stderr)
+
+# with open(os.path.join(app.config['DATA_DIR'], 'stores.json')) as json_file:
+
+        # stores.sort() # TypeError: '<' not supported between instances of 'dict' and 'dict'
+        # print('json loaded', file=sys.stderr)
+
+        # print("town: %s, postcode: %s" % (name.replace('_', ' '), stores[name]), file=sys.stderr)
+        # print("<tr><td>%s</td><td>%s</td></tr>" % (name, stores[name]))
