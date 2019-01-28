629dc8fc5b9544fe4c709b2bb2760e658d19d134
Chris Bird <drkvogel@yahoo.co.uk>
2019-01-28 01:55:14 +0000

    display sorted list of places and postcodes

--- app/app.py
+++ app/app.py
@@ -3,45 +3,42 @@ from flask.json import jsonify
 import os
 import sys
 import json
+from pprint import pprint
 
 app = Flask(__name__)
 
 # app.config['DATA_DIR'] = '.'
 
-cache = {}
+# cache = {}
 
-json_data = None
+stores = {}
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 @app.route("/test")
 def test():
-    # return render_template('index.html')
     get_config()
-    print('test2', file=sys.stderr) # prints to console
-    print(json_data, file=sys.stderr) # prints to console
     return "stuff"
 
 @app.route("/stores")
-def stores():
+def list_stores():
     get_config()
-    print('/stores', file=sys.stderr) # prints to console
-    # print(json_data, file=sys.stderr) # prints to console
-    # return json_data
-    for row in json_data:
-        print("town: %s, postcode: %s" % (row['name'], row['postcode']), file=sys.stderr) # prints to console
-    return render_template('stores.html', stores=json_data)
+    # print('/stores', file=sys.stderr)
+    # pprint(stores) # prints sorted version of stores??
+    rows = ''
+    for name in sorted(stores):
+        rows = rows + "<tr><td>%s</td><td>%s</td></tr>\n" % (name, stores[name])
+    return render_template('stores.html', stores=stores, rows=rows)
 
 def get_config():
-    global json_data # !!
-    # with open(os.path.join(app.config['DATA_DIR'], 'stores.json')) as json_file:
+    global stores # !!
     with open('stores.json') as json_file:
-        json_data = json.load(json_file)
-        print('json loaded', file=sys.stderr)
+        rows = json.load(json_file) # dict of dicts
+        for row in rows:
+            stores[row['name']] = row['postcode']   # new dict
 
 if __name__ == '__main__':
-    # print('test1', file=sys.stderr)
     get_config()
     app.run(debug=True)
